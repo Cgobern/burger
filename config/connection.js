44831457fdffa68599f9e61406a46159d7f69f28
Cortney Gobern <CortneyGobern@Cortneys-MacBook-Pro.local>
2016-10-01 17:31:54 -0400

    changed a few things in connection file

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,15 @@ Here is where you make the connection to the database and export and used by the
 */
 var mysql = require('mysql');
 var connection;
+
 if (process.env.JAWSDB_URL){
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
-	mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'burgers_db'
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		database: 'burgers_db'
 	});
 }
 connection.connect(function (err) {
